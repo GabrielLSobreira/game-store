bcb6aeac8950931a1ac373a93964e58a28b4bc82
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-12 13:35:34 -0300

    Página de detalhes do jogo

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,97 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetServerSideProps } from 'next';
+import { RichText } from 'prismic-dom';
+import { Game } from '../types/Game';
+import { getPrismicClient } from '../services/prismic';
+import {
+  Container,
+  GameInfo,
+  GameInfoContainer,
+  Plataforms,
+  Price,
+  Resume,
+} from '../styles/gamedetails';
+import { FaXbox, FaPlaystation } from 'react-icons/fa';
+import { BsArrowLeftCircle } from 'react-icons/bs';
+import Link from 'next/link';
+
+interface gameProps {
+  game: Game;
+}
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+const GameDetails = ({ game }: gameProps) => {
+  return (
+    <Container>
+      <Link href="/">
+        <a>
+          <BsArrowLeftCircle /> Voltar
+        </a>
+      </Link>
+      <h2>{game.title}</h2>
+      <GameInfoContainer>
+        <img src={game.image.url} alt={game.title} />
+        <GameInfo>
+          <strong>Sinopse: </strong>
+          <p dangerouslySetInnerHTML={{ __html: game.content }}></p>
+          <span>
+            <strong>Data de Lançamento: </strong>
+            {game.date}
+          </span>
+          <Plataforms>
+            <strong>
+              Plataformas:
+              <FaXbox /> <FaPlaystation />
+            </strong>
+          </Plataforms>
+          <Price>
+            <strong>R${game.price.toFixed(2).replace('.', ',')}</strong>
+            <span>
+              3x de R${(game.price / 3).toFixed(2).replace('.', ',')} sem juros
+            </span>
+            <span>
+              <strong>
+                R$
+                {(game.price - game.price * 0.05)
+                  .toFixed(2)
+                  .replace('.', ',')}{' '}
+              </strong>
+              á vista
+            </span>
+            <button>Comprar</button>
+          </Price>
+        </GameInfo>
+      </GameInfoContainer>
+    </Container>
+  );
+};
+
+export const getServerSideProps = async ({ params }: Params) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('game', String(slug), {});
+  const game = {
+    slug: response.uid,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    date: new Date(response.data.date).toLocaleDateString('pt-br', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+    image: response.data.image,
+    price: response.data.price,
+  };
+
+  return {
+    props: {
+      game,
+    },
+  };
+};
+
+export default GameDetails;
---
+++ src/styles/gamedetails.ts
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+
+export const GameInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: 1000px;
+
+  img {
+    width: 400px;
+    border-radius: 5px;
+    box-shadow: 0 0 5px black;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      width: 150px;
+      margin-bottom: 2rem;
+    }
+  }
+`;
+
+export const GameInfo = styled.div`
+  strong {
+    font-size: 1.3rem;
+  }
+  p {
+    margin-bottom: 1.5rem;
+    font-size: 1.2rem;
+  }
+
+  span {
+    display: block;
+    font-size: 1.3rem;
+  }
+`;
+
+export const Plataforms = styled.div`
+  margin-top: 1.5rem;
+
+  svg:first-child {
+    margin-left: 0.5rem;
+  }
+
+  svg + svg {
+    margin-left: 0.3rem;
+  }
+`;
+
+export const Resume = styled.div`
+  display: flex;
+`;
+
+export const Price = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    font-size: 1.5rem;
+    margin-top: 1.5rem;
+    margin-bottom: 0.2rem;
+  }
+
+  span {
+    font-size: 1rem;
+    margin-bottom: 0.2rem;
+
+    strong {
+      font-size: 1rem;
+    }
+  }
+
+  button {
+    width: 80%;
+    padding: 1rem 0;
+    font-size: 1.5rem;
+    margin: 0.5rem 0;
+    border: none;
+    background-color: #61ce70;
+    border-radius: 5px;
+    color: #fff;
+    transition: all 0.4s;
+
+    &:hover {
+      background-color: #28ef42;
+    }
+
+    @media (max-width: 1024px) {
+      max-width: 50%;
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 2rem;
+  h2 {
+    margin-bottom: 2rem;
+    font-size: 1.8rem;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    margin: 1rem 0;
+    svg {
+      margin-right: 0.3rem;
+    }
+  }
+`;
