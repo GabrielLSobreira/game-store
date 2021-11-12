ad2f2359aa7c522ee2b5dc52a924420ce990eeff
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-12 08:48:18 -0300

    Estilos Globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
